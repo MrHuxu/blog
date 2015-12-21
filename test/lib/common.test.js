cf6439bc6da67212c8d78672799f0db65d853397
Xu Hu <hxtheone@gmail.com>
2015-12-21 22:41:42 +0800

    add two more test case

--- test/lib/common.test.js
+++ test/lib/common.test.js
@@ -2,7 +2,7 @@ var common = require('../../lib/common');
 
 var expect = require('chai').expect;
 
-describe('Test functions in lib/common.js', () => {
+describe('Test primary functions in lib/common.js', () => {
   it('[func] prd/dev env check', () => {
     expect(common.onProd()).to.not.be.ok;
   });
@@ -18,5 +18,25 @@ describe('Test functions in lib/common.js', () => {
       '20151220',
       'JavaScript-Promise-Closuer.md'
     ]);
+  });
+});
+
+describe('Test get infos from a file name', () => {
+  var nameArr;
+
+  beforeEach(()=> {
+    nameArr = ['67', '当Promise遇上闭包', '20151220', 'JavaScript-Promise-Closuer.md'];
+  });
+
+  it('[func] get article time', () => {
+    expect(common.getArticleTime(nameArr)).to.be.deep.equal({
+      year  : '2015',
+      month : '12',
+      day   : '20'
+    });
+  });
+
+  it('[func] get article tags', () => {
+    expect(common.getArticleTags(nameArr)).to.be.deep.equal(['JavaScript', 'Promise', 'Closuer']);
   });
 });
