1f118b8c9e2d06659cd0f2f246e781fd5cc71469
Xu Hu <hxtheone@gmail.com>
2016-01-18 02:01:13 +0800

    update get cached/get repo logic

--- routes/project.js
+++ routes/project.js
@@ -1,12 +1,13 @@
 require("babel-core/register");
 import { Router } from 'express';
 import { getRepos } from '../lib/get-repos';
+import { clearEmptyObjItems } from '../lib/common';
 
 var router = Router();
 
 router.get('/repos', (req, res) => {
   getRepos().then((repos) => {
-    res.send({entities: repos});
+    res.send({entities: clearEmptyObjItems(repos)});
   });
 });
 
