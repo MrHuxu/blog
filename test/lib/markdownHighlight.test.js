a8b91609a68aca7fb3f7eeaf8f44bda191805602
MrHuxu <hxtheone@gmail.com>
2017-09-29 15:00:02 +0800

    fix ut

--- test/lib/markdownHighlight.test.js
+++ test/lib/markdownHighlight.test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 describe('Test primary functions in lib/markdown-highlight.js', () => {
   it('[func] decode and highlight markdown doc', () => {
     expect(markdownHighlight("## test\n    p 'hello world'")).to.be.equal(
-      '<h2 id="test">test</h2>\n<pre><code class="hljs"><span class="hljs-selector-tag">p</span> <span class="hljs-string">&apos;hello world&apos;</span>\n</code><ul class="numbering"><li>1</li></ul></pre>'
+      '<h2 id="test">test</h2>\n<pre><code class="hljs"><span class="hljs-selector-tag">p</span> <span class="hljs-string">&apos;hello world&apos;</span>\n</code><ul class="numbering"><div>1</div></ul></pre>'
     );
   });
 });
